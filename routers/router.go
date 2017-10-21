063a6f18733b6dce614ff17f50789da80099b57e
wph95 <wph657856467@gmail.com>
2017-10-22 07:00:58 +0800

    fix router and conftrollers error

--- routers/router.go
+++ routers/router.go
@@ -10,4 +10,6 @@ func init() {
     beego.Router("/", &controllers.MainController{})
 	beego.Router("/storage", &controllers.StorageController{})
 	beego.Router("/storage/show", &controllers.StorageShowController{})
+	beego.Router("/api/file/list", &controllers.GetFileController{})
+	beego.Router("/api/block/list", &controllers.GetFileBlocksListController{})
 }
