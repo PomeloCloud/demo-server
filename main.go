205f3190016680307787d37794251f459d8b8f05
Hao Shi <shisoftgenius@gmail.com>
2017-10-22 12:48:03 +0800

    fix cross origin

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/PomeloCloud/pcfs/drone/storage"
 	"os"
+	"github.com/astaxie/beego/plugins/cors"
 )
 
 func cleanup() {
@@ -15,6 +16,20 @@ func cleanup() {
 func main() {
 	cleanup()
 	storage.Main()
+	beego.Debug("Filters init...")
+
+	// CORS for https://foo.* origins, allowing:
+	// - PUT and PATCH methods
+	// - Origin header
+	// - Credentials share
+
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
+		AllowCredentials: true,
+	}))
 	beego.Run()
 }
 
